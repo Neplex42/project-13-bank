08ee62fee9711479dde7a53488ca2ed55a906030
JordanDavidAx <jdavid@axome.com>
2025-02-16 17:33:27 +0100

    added try catch on service

--- src/services/auth/authService.js
+++ src/services/auth/authService.js
@@ -1,28 +1,43 @@
 import axios from 'axios'
 
+const backendURL = 'http://localhost:3001/api/v1'
+
 const logout = () => {
   localStorage.removeItem('userToken')
 }
-const backendURL = 'http://localhost:3001/api/v1'
 
 const login = async (userData) => {
-  const response = await axios.post(`${backendURL}/user/login`, userData)
+  try {
+    const response = await axios.post(`${backendURL}/user/login`, userData);
 
-  if (response.data) {
-    localStorage.setItem('userToken', response.data.body.token)
+    if (response.data && response.data.body && response.data.body.token) {
+      localStorage.setItem('userToken', response.data.body.token);
+    }
+
+    return response.data;
+  } catch (error) {
+    console.error('Error during login:', error);
+    return error.response || error;
   }
+};
 
-  return response.data
-}
 
 const getUserProfile = async (token) => {
-  const response = await axios.post(`${backendURL}/user/profile`, {}, {
-    headers: {
-      authorization: `Bearer ${token}`
+  try {
+    const response = await axios.post(`${backendURL}/user/profile`, {}, {
+      headers: {
+        authorization: `Bearer ${token}`
+      }
+    })
+    if (response.status === 401 || response.status === 403) {
+      localStorage.removeItem('userToken')
     }
-  })
 
-  return response.data
+    return response.data
+  } catch (error) {
+    console.error('Error in authService.js', error)
+    throw new Error(error)
+  }
 }
 
 export const authService = {
